90d20d8957d3d257e051c1a178eb53c130b81eb9
MKR404-CYBER <115325635+Mkr404-Cyber@users.noreply.github.com>
2023-01-10 22:31:48 +0600

    Create DUMP.py

---
+++ DUMP.py
@@ -0,0 +1,14 @@
+import os, platform, time
+try:os.system('git pull')
+except:pass
+try:os.system('am start https://facebook.com/groups/552401094807535/')
+except:pass
+try:print(" [+] CRACK BY MKR ");time.sleep(3)
+except:pass
+import requests                                                                         bit = platform.architecture()[0]
+if bit == '64bit':
+    from Sarfraz import menu
+    menu()
+elif bit == '32bit':
+    from Sarfraz32 import menu
+    menu()
